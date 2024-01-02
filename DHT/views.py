6db026dbe789bca4199afab5ade2ab6d8f53a8d8
Aya Bouri <aya.bouri1@gmail.com>
2024-01-02 16:20:58 +0100

    Add Commit

--- DHT/views.py
+++ DHT/views.py
@@ -8,6 +8,19 @@ from django.http import JsonResponse
 from datetime import timedelta
 import datetime
 import telepot
+from django.shortcuts import render
+from .models import Dht11  # Assurez-vous d'importer le modèle Dht11
+from django.utils import timezone
+import csv
+from django.http import HttpResponse
+from django.utils import timezone
+from django.http import JsonResponse
+from datetime import timedelta
+import datetime
+import telepot
+from django.contrib.auth import authenticate, login,logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 def table(request):
     derniere_ligne = Dht11.objects.last()
     derniere_date = Dht11.objects.last().dt
@@ -106,6 +119,109 @@ def chart_data_mois(request):
 
 def sendtele():
     token = '5788112878:AAG_Gcvrl64MpT6qQT59KonBXR3LeXo3Cwg'
+    rece_id = 1242839034
+    bot = telepot.Bot(token)
+    bot.sendMessage(rece_id, 'la température depasse la normale')
+    print(bot.sendMessage(rece_id, 'OK.'))
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticating the user
+        user = authenticate(request, username=username, password=password)
+        # Checking if authentication is successful
+        if user is not None:
+            login(request, user)
+            return redirect("/index")
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+
+    return render(request, 'login.html')
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+@login_required
+def table(request):
+    derniere_ligne = Dht11.objects.last()
+    derniere_date = Dht11.objects.last().dt
+    delta_temps = timezone.now() - derniere_date
+    difference_minutes = delta_temps.seconds // 60
+    temps_ecoule = ' il y a ' + str(difference_minutes) + ' min'
+    if difference_minutes > 60:
+        temps_ecoule = 'il y ' + str(difference_minutes // 60) + 'h' + str(difference_minutes % 60) + 'min'
+    valeurs = {'date': temps_ecoule, 'id': derniere_ligne.id, 'temp': derniere_ligne.temp, 'hum': derniere_ligne.hum}
+    return render(request, 'value.html', {'valeurs': valeurs})
+@login_required
+def download_csv(request):
+    model_values = Dht11.objects.all()
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="dht.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['id', 'temp', 'hum', 'dt'])
+    liste = model_values.values_list('id', 'temp', 'hum', 'dt')
+    for row in liste:
+        writer.writerow(row)
+    return response
+#pour afficher navbar de template
+@login_required
+def index_view(request):
+    return render(request, 'index.html')
+@login_required
+def graphique(request):
+    return render(request, 'Chart.html')
+# récupérer toutes les valeur de température et humidity sous forme un #fichier json
+@login_required
+def chart_data(request):
+    dht = Dht11.objects.all()
+
+    data = {
+        'temps': [Dt.dt for Dt in dht],
+        'temperature': [Temp.temp for Temp in dht],
+        'humidity': [Hum.hum for Hum in dht]
+    }
+    return JsonResponse(data)
+
+#pour récupérer les valeurs de température et humidité de dernier 24h
+# et envoie sous forme JSON
+@login_required
+def chart_data_jour(request):
+    dht = Dht11.objects.all()
+    now = timezone.now()
+
+    # Récupérer l'heure il y a 24 heures
+    last_24_hours = now - timezone.timedelta(hours=24)
+
+    # Récupérer tous les objets de Module créés au cours des 24 dernières heures
+    dht = Dht11.objects.filter(dt__range=(last_24_hours, now))
+    data = {
+        'temps': [Dt.dt for Dt in dht],
+        'temperature': [Temp.temp for Temp in dht],
+        'humidity': [Hum.hum for Hum in dht]
+    }
+    return JsonResponse(data)
+
+#pour récupérer les valeurs de température et humidité de dernier semaine
+# et envoie sous forme JSON
+@login_required
+def chart_data_mois(request):
+    dht = Dht11.objects.all()
+
+    date_debut_semaine = timezone.now().date() - datetime.timedelta(days=30)
+    print(datetime.timedelta(days=30))
+    print(date_debut_semaine)
+
+    # filtrer les enregistrements créés depuis le début de la semaine dernière
+    dht = Dht11.objects.filter(dt__gte=date_debut_semaine)
+
+    data = {
+        'temps': [Dt.dt for Dt in dht],
+        'temperature': [Temp.temp for Temp in dht],
+        'humidity': [Hum.hum for Hum in dht]
+    }
+    return JsonResponse(data)
+
+def sendtele():
+    token = '6662023260:AAG4z48OO9gL8A6szdxg0SOma5hv9gIII1E'
     rece_id = 1242839034
     bot = telepot.Bot(token)
     bot.sendMessage(rece_id, 'la température depasse la normale')
